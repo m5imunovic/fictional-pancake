058dcec3f1e5a4352a609f10387e00fd1b7e16d8
m5imunovic <simunovic.marijo@gmail.com>
2024-06-19 00:10:51 +0200

    Split dbg lightning module for classification and regression

--- src/models/dbg_regression_module.py
+++ src/models/dbg_regression_module.py
@@ -3,17 +3,12 @@ import numpy as np
 import pytorch_lightning as pl
 import torch
 import torch.nn as nn
-from torchmetrics.classification import (
-    BinaryAccuracy,
-    BinaryConfusionMatrix,
-)
 from torchmetrics.regression import MeanSquaredError
 
-from eval.inference_metrics import InferenceMetrics
-from models.loss.rmse_loss import RMSELoss
+from models.loss.mixture_loss import MixtureLoss
 
 
-class DBGLightningModule(pl.LightningModule):
+class DBGRegressionModule(pl.LightningModule):
     def __init__(
         self,
         net: nn.Module,
@@ -21,7 +16,6 @@ class DBGLightningModule(pl.LightningModule):
         scheduler: torch.optim.lr_scheduler.ReduceLROnPlateau,
         criterion: torch.nn.modules.loss._Loss,
         batch_size: int = 1,
-        threshold: float = 0.5,
         offset: int = 0.5,  # in regression setting we use this to adapt range for softmax operation
         storage_path: Path | None = None,
     ):
@@ -31,20 +25,12 @@ class DBGLightningModule(pl.LightningModule):
         self.net = net
         self.batch_size = batch_size
 
-        if self.regression_mode():
-            # training metrics
-            self.train_acc = MeanSquaredError()
-            # validation metrics
-            self.val_acc = MeanSquaredError()
-        else:
-            # training metrics
-            self.train_acc = BinaryAccuracy()
-            # validation metrics
-            self.val_acc = BinaryAccuracy()
+        # training metrics
+        self.train_metric = MeanSquaredError()
+        # validation metrics
+        self.val_metric = MeanSquaredError()
 
         # test metrics
-        self.test_metrics = InferenceMetrics(threshold=threshold)
-        self.cm = BinaryConfusionMatrix(threshold=threshold)
         self.storage_path = storage_path
 
     def configure_optimizers(self) -> torch.optim.Optimizer:
@@ -66,8 +52,7 @@ class DBGLightningModule(pl.LightningModule):
         edge_attr = getattr(batch.data, "edge_attr", None)
 
         scores = self.net(x=x.float(), edge_index=edge_index, edge_attr=edge_attr)
-        if self.regression_mode():
-            scores = torch.clamp(scores, min=0)
+        # scores = torch.clamp(scores, min=0)
 
         if getattr(batch.data, "y") is not None:
             y = batch.data.y
@@ -78,11 +63,11 @@ class DBGLightningModule(pl.LightningModule):
 
     def training_step(self, batch, batch_idx, dataloader_idx=0):
         scores, expected_scores = self.common_step(batch, batch_idx, dataloader_idx)
-        loss = self.hparams.criterion(scores, expected_scores)
-        if self.regression_mode():
-            self.train_acc(scores, expected_scores.int())
+        if self.uses_mixture_loss():
+            loss = self.hparams.criterion(batch.data.edge_index, scores, expected_scores)
         else:
-            self.train_acc(torch.sigmoid(scores), expected_scores.int())
+            loss = self.hparams.criterion(scores, expected_scores)
+        self.train_metric(scores, expected_scores.int())
 
         self.log(
             "train/loss",
@@ -96,24 +81,24 @@ class DBGLightningModule(pl.LightningModule):
         return loss
 
     def on_train_epoch_end(self) -> None:
-        output = self.train_acc.compute()
+        output = self.train_metric.compute()
         self.log(
-            "train/acc",
+            "train/mse",
             output,
             on_epoch=True,
             prog_bar=True,
             logger=True,
             batch_size=self.batch_size,
         )
-        self.train_acc.reset()
+        self.train_metric.reset()
 
     def validation_step(self, batch, batch_idx, dataloader_idx=0):
         scores, expected_scores = self.common_step(batch, batch_idx, dataloader_idx)
-        loss = self.hparams.criterion(scores, expected_scores)
-        if self.regression_mode():
-            self.val_acc.update(scores, expected_scores.int())
+        if self.uses_mixture_loss():
+            loss = self.hparams.criterion(batch.data.edge_index, scores, expected_scores)
         else:
-            self.val_acc.update(torch.sigmoid(scores), expected_scores.int())
+            loss = self.hparams.criterion(scores, expected_scores)
+        self.val_metric.update(scores, expected_scores.int())
 
         self.log(
             "val/loss",
@@ -128,9 +113,9 @@ class DBGLightningModule(pl.LightningModule):
         return loss
 
     def on_validation_epoch_end(self):
-        output = self.val_acc.compute()
+        output = self.val_metric.compute()
         self.log(
-            "val/acc",
+            "val/mse",
             output,
             on_epoch=True,
             prog_bar=True,
@@ -138,47 +123,34 @@ class DBGLightningModule(pl.LightningModule):
             batch_size=self.batch_size,
             add_dataloader_idx=True,
         )
-        self.val_acc.reset()
+        self.val_metric.reset()
 
     def test_step(self, batch, batch_idx, dataloader_idx=0):
         scores, expected_scores = self.common_step(batch, batch_idx, dataloader_idx)
-        if self.regression_mode():
-            # We define a hyperparameter offset which shifts the range to negative values
-            # After that we will normalize with sigmoid so everything negative becomes "falsy"
-            # This way we can still use metrics as for classification case
-            scores = scores - self.hparams.offset
-        else:
-            scores = torch.sigmoid(scores)
+        # We define a hyperparameter offset which shifts the range to negative values
+        # After that we will normalize with sigmoid so everything negative becomes "falsy"
+        # This way we can still use metrics as for classification case
+        scores = scores - self.hparams.offset
         scores = scores.reshape((1, -1))
         expected_scores = expected_scores.reshape((1, -1))
         if self.storage_path is not None:
             np.save(f"{self.storage_path}/{batch.path[0].stem}", scores.cpu().numpy())
             np.save(f"{self.storage_path}/expected_{batch.path[0].stem}", expected_scores.cpu().numpy())
             torch.save(batch.data, f"{self.storage_path}/transformed_{batch.path[0].stem}.pt")
-        self.test_metrics.update(scores, expected_scores.int(), batch.path)
-        self.cm(scores, expected_scores.int())
 
     def on_test_end(self):
         return
-        df = self.test_metrics.finalize()
-        print(df.to_string())
-        if hasattr(self.logger, "log_table"):
-            self.logger.log_table("test/metrics", dataframe=df)
-        print(self.cm.compute())
 
     def predict_step(self, batch, batch_idx, dataloader_idx=0):
         scores, _ = self.common_step(batch, batch_idx, dataloader_idx)
-        if self.regression_mode():
-            # We define a hyperparameter offset which shifts the range to negative values
-            # After that we will normalize with sigmoid so everything negative becomes "falsy"
-            # This way we can still use metrics as for classification case
-            scores = scores - self.hparams.offset
-            scores = torch.clamp(scores, min=0)
-        else:
-            scores = torch.sigmoid(scores).reshape((1, -1))
+        # We define a hyperparameter offset which shifts the range to negative values
+        # After that we will normalize with sigmoid so everything negative becomes "falsy"
+        # This way we can still use metrics as for classification case
+        scores = scores - self.hparams.offset
+        scores = torch.clamp(scores, min=0)
         if self.storage_path is not None:
             torch.save(scores.cpu(), f"{self.storage_path}/{batch.path[0].stem}.pt")
         return scores
 
-    def regression_mode(self):
-        return isinstance(self.hparams.criterion, RMSELoss) or isinstance(self.hparams.criterion, nn.PoissonNLLLoss)
+    def uses_mixture_loss(self):
+        return isinstance(self.hparams.criterion, MixtureLoss)
--- test/unit/test_train_regression.py
+++ test/unit/test_train_regression.py
@@ -1,10 +1,13 @@
+from unittest import mock
 from pathlib import Path
 
 from train import train
 
 
-def test_train(test_train_regress_cfg):
+@mock.patch("train.upload_model_to_wandb")
+def test_train(mock_upload_model, test_train_regress_cfg):
     train(test_train_regress_cfg)
     expected_model_path = Path(test_train_regress_cfg.model_output_path) / "best_model.ckpt"
     assert expected_model_path.exists()
     # TODO: assert that the wandb is handled properly
+    assert mock_upload_model.call_count == 1
